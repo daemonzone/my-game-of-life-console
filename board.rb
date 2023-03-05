d16f462fa98097e4e09ad120795330069950b89c
Davide <davide@daemonzone.it>
2023-03-05 20:00:02 +0100

    Rspec new version (should => expect) Testing on all neighbours for any cell Board.nextgen

--- board.rb
+++ board.rb
@@ -1,8 +1,15 @@
 class Board
 
 	attr_accessor :cells
-	
+
 	def initialize
 		@cells = []
 	end
+
+	def nextgen
+		cells.each do |cell|
+			cell.die if cell.neighbours.count < 2
+		end
+	end
+
 end
--- cell.rb
+++ cell.rb
@@ -9,12 +9,36 @@ class Cell
   	board.cells << self
   end
 
-  def neighbors
+  def neighbours
+  	@neighbours = []
+  	board.cells.each do |cell|
+  		(@neighbours << cell) if self.x == cell.x + 1 && self.y == cell.y - 1 	 # NW (-1,1)
+  		(@neighbours << cell) if self.x == cell.x && self.y == cell.y - 1 			 # N  (0,1)
+  		(@neighbours << cell) if self.x == cell.x - 1 && self.y == cell.y - 1 	 # NE (1,1)
+  		(@neighbours << cell) if self.x == cell.x - 1 && self.y == cell.y 	 		 # E (1,0)
+  		(@neighbours << cell) if self.x == cell.x - 1 && self.y == cell.y + 1 	 # SE (1,-1)
+  		(@neighbours << cell) if self.x == cell.x && self.y == cell.y + 1 			 # S  (0,-1)
+  		(@neighbours << cell) if self.x == cell.x + 1 && self.y == cell.y + 1 	 # SW (-1,-1)
+  		(@neighbours << cell) if self.x == cell.x + 1 && self.y == cell.y 			 # W (-1,0)
+  	end
   	
+  	@neighbours
   end
 
   def create_at(x, y)
-  	Cell.new(x, y)
+  	Cell.new(board, x, y)
+  end
+
+  def die
+  	 board.cells -= [self]
+  end
+
+  def dead?
+  	!board.cells.include?(self)
+  end
+
+  def alive?
+  	board.cells.include?(self)
   end
 
 end
